1fc7252c2d6606f8e18ea2c2be2ba277868adea8
Vlad78 <dssdom@gmail.com>
2023-03-12 15:04:35 +0300

    feat: connected MongoDB

--- index.ts
+++ index.ts
@@ -1,5 +1,15 @@
 import express from "express";
 import jwt from "jsonwebtoken";
+import mongoose from "mongoose";
+
+mongoose
+  .connect("mongodb+srv://dssdom:wwwwww@cluster0.7xdu4do.mongodb.net/?retryWrites=true&w=majority")
+  .then((res) => {
+    console.log("BD connected successfully");
+  })
+  .catch((err) => {
+    console.log("err", err);
+  });
 
 const app = express();
 
@@ -7,6 +17,22 @@ app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("It works");
+});
+
+app.post("/login", (req, res) => {
+  const token = jwt.sign(
+    {
+      email: req.body.email,
+      password: req.body.password,
+    },
+    "secretKey"
+  );
+  console.log(req.body);
+
+  res.json({
+    token: token,
+    email: req.body.email,
+  });
 });
 
 app.listen(3000, () => {
